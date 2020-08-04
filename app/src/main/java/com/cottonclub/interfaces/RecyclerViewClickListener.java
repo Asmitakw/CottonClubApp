28975cb51d6e2e8bee11bc3df000b585f10dd8f5
asmitawakchaure93 <asmita.wakchaure04@gmail.com>
2020-08-04 12:13:02 +0530

    Added Edit/Update Order Functionality

---
+++ app/src/main/java/com/cottonclub/interfaces/RecyclerViewClickListener.java
@@ -0,0 +1,11 @@
+package com.swarveda.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by Asmita on 11-08-2016.
+ */
+    public interface RecyclerViewClickListener {
+        void onClick(View view, int position);
+    }
+
